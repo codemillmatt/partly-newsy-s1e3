0324d74eb2371698152698a0270688590c7237a0
Matt Soucoup <msoucoup@codemilltech.com>
2019-08-13 13:49:49 -0700

    Final

---
+++ PartlyNewsy.Core/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using PartlyNewsy.Core;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PartlyNewsy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShellPage();
+
+            Routing.RegisterRoute("articledetail", typeof(ArticleDetailPage));
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ PartlyNewsy.Core/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml;
+using Xamarin.Forms;
+
+namespace PartlyNewsy.Core
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await GetData();
+        }
+
+        async Task GetData()
+        {
+            var svc = new NewsService();
+
+            var articles = await svc.GetTopNews();
+
+            newsList.ItemsSource = articles;
+        }
+    }
+}
